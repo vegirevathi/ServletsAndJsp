26568d0b9a3c99efa8860e9153e7f44fcac38627
Revathi <vegirevathi@gmail.com>
2022-06-23 21:30:01 +0530

    Servlets basics

--- DemoApp/src/com/practice/AddServlet.java
+++ DemoApp/src/com/practice/AddServlet.java
@@ -3,18 +3,39 @@ package com.practice;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		int k = i + j;
-		PrintWriter out = res.getWriter();
-		out.print("Result is : "+k);
+		
+		//Using Request Dispatcher
+//		req.setAttribute("sum", k);
+//		RequestDispatcher rd=  req.getRequestDispatcher("sq");
+//		rd.forward(req, res);
+		
+		//Using sendRedirect
+		//res.sendRedirect("sq?k=" + k); //this is called URL rewriting
+		
+		
+		//Using HttpSession
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k);
+//		res.sendRedirect("sq");
+		
+		//Using cookies
+		Cookie cookie = new Cookie("k", String.valueOf(k));
+		res.addCookie(cookie);
+		res.sendRedirect("sq");
 	}
 
 }
---
+++ DemoApp/src/com/practice/SquareServlet.java
@@ -0,0 +1,59 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+
+public class SquareServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		//Using Request Dispatcher		
+//		int i = (int)req.getAttribute("sum");
+//		int j = i * i;
+		
+		
+		//Using sendRedirect method
+//		int i = Integer.parseInt(req.getParameter("k"));
+//		int j = i * i;
+//		PrintWriter out = res.getWriter();
+//		out.print("Result is : "+j);
+		
+		//Using HttpSession
+//		HttpSession session = req.getSession();
+//		int i = (int)session.getAttribute("k");
+//		int j = i * i;
+//		PrintWriter out = res.getWriter();
+//		out.print("Result is : "+j);
+		
+		//Using Cookies
+		int i = 0;
+		Cookie[] cookies = req.getCookies();
+		for(Cookie c : cookies) {
+			if(c.getName().equals("k")) {
+				i = Integer.parseInt(c.getValue());
+			}
+		}
+		
+		int j = i * i;
+		PrintWriter out = res.getWriter();
+		out.print("Result is : "+j);
+		
+		//Concept of servlet context - Added context-param in web.xml
+		ServletContext sc = getServletContext();
+		out.println("My name is "+ sc.getInitParameter("name"));
+		
+		//Concept of servlet config - Added init-param in web.xml inside the square servlet tag
+		ServletConfig config = getServletConfig();
+		out.println("my name in this servlet is "+config.getInitParameter("name"));
+	}
+}
